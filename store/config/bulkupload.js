abb61b4dbd02447022385707de48b04749040d27
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-05-10 23:44:44 +0600

    pppp

--- store/config/bulkupload.js
+++ store/config/bulkupload.js
@@ -1,13 +1,13 @@
 import { collection, doc, setDoc } from "firebase/firestore";
-import { carouselImages } from "../restaurants";
+import { slots } from "../restaurants";
 import { db } from "./firebaseConfig";
 
-const restaurestData = carouselImages
+const restaurestData = slots
 const uploadData = async () =>{
     try{
         for(let i = 0 ; i < restaurestData.length; i++){
           const restaurant = restaurestData[i]  
-          const docRef = doc(collection(db, "carousel"), `carousel_${i+1}`);
+          const docRef = doc(collection(db, "slots"), `slots_${i+1}`);
           await setDoc(docRef, restaurant)
         }
         console.log("Data uploaded successfully")
